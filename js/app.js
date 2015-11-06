91b96fb7e27db7747f00fa259789c100023d2a2e
James Newman <jamesmnewman1@gmail.com>
2015-11-05 22:59:35 -0800

    setting routes

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 // main js file of app
 var App = angular.module('marketplace', ['ui.router']);
+
+App.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+		// states are the routes
+		.state('home', {
+			url: '/home',
+			templateUrl: 'home/_home.html',
+			controller: 'MainCtrl'
+		})
+	$urlRouterProvider.otherwise('home');	
+}])
