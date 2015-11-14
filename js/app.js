e545b115e2d2f0c90115f3e7fc2431e3b37644b0
James Newman <jamesmnewman1@gmail.com>
2015-11-13 22:12:17 -0800

    controller

--- js/app.js
+++ js/app.js
@@ -12,25 +12,25 @@ App.config([
 			templateUrl: 'home/_home.html',
 			controller: 'MainCtrl',
 		})
-		.state('login', {
-			url: '/login',
-			templateUrl: 'auth/_login.html',
-			controller: 'AuthCtrl',
-			onEnter: ['$state', 'Auth', function($state, Auth) {
-            Auth.currentUser().then(function (){
-            $state.go('home');
-           })
-          }]
-		})
-		.state('register', {
-			url: '/register',
-			templateUrl: 'auth/_register.html',
-			controller: 'AuthCtrl',
-			onEnter: ['$state', 'Auth', function($state, Auth) {
-            Auth.currentUser().then(function (){
-            $state.go('home');
-            })
-          }]
-		})
-	$urlRouterProvider.otherwise('home');	
-}])
+	// 	.state('login', {
+	// 		url: '/login',
+	// 		templateUrl: 'auth/_login.html',
+	// 		controller: 'AuthCtrl',
+	// 		onEnter: ['$state', 'Auth', function($state, Auth) {
+ //            Auth.currentUser().then(function (){
+ //            $state.go('home');
+ //           })
+ //          }]
+	// 	})
+	// 	.state('register', {
+	// 		url: '/register',
+	// 		templateUrl: 'auth/_register.html',
+	// 		controller: 'AuthCtrl',
+	// 		onEnter: ['$state', 'Auth', function($state, Auth) {
+ //            Auth.currentUser().then(function (){
+ //            $state.go('home');
+ //            })
+ //          }]
+	// 	})
+	// $urlRouterProvider.otherwise('home');	
+}]);
---
+++ js/home/MainCtrl.js
@@ -0,0 +1,7 @@
+App.controller('MainCtrl', function($scope){
+    $scope.names = [
+        {id: 1, name: 'Arthur'},
+        {id: 2, name: 'Portos'},
+        {id: 3, name: 'Aramis'}
+    ];
+});
